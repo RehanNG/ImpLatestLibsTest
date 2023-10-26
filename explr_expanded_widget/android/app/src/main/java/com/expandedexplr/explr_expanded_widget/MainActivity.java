2201fef7caed21c170856daa801f55d48de40db8
RehanNG <rehanusmaning@gmail.com>
2023-10-26 15:09:29 +0500

    Tested Expanded Widget
    Tested Expanded Widget in flutter , here Expanded widget is being tested on Container

---
+++ explr_expanded_widget/android/app/src/main/java/com/expandedexplr/explr_expanded_widget/MainActivity.java
@@ -0,0 +1,6 @@
+package com.expandedexplr.explr_expanded_widget;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ explr_expanded_widget/lib/expanded_widget_test/test1.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+class expt1 extends StatefulWidget {
+  const expt1({super.key});
+
+  @override
+  State<expt1> createState() => _expt1State();
+}
+
+class _expt1State extends State<expt1> {
+  @override
+  Widget build(BuildContext context) {
+    return   Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        children: [
+
+          Row(
+            children: [
+              Expanded(child: Container(
+                width: 200,
+                height: 200,
+                color: Color(0xFFF35353),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.yellow,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.yellow,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.yellow,
+                          ),
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.yellow,
+                          ),
+                        ),
+
+                      ],
+                    ),
+                  ),
+                ),
+              )),
+              Expanded(
+                  child: Container(
+                width: 200,
+                height: 200,
+                color: Color(0xFF35FD91),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 30 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                          Text("data swfsdfds sdf sd fsdfsdf sdf",style: TextStyle( fontSize: 20 ),),
+                        ],
+                      ),
+                    ),
+              )),
+
+            ],
+          )
+
+        ],
+        ),
+    );
+
+
+  }
+}
